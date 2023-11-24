02e8d351936f7d290bf37b1c6287b26094923c53
saidawh <saidawahmane17@gmail.com>
2023-11-24 17:30:50 +0100

    second commit

--- script.js
+++ script.js
@@ -11,6 +11,41 @@ function clearDisplay() {
   display.innerText = "0";
 }
 
+function calculateSquareRoot() {
+  try {
+    currentInput = Math.sqrt(eval(currentInput)).toString();
+    display.innerText = currentInput;
+  } catch (error) {
+    display.innerText = "Error";
+  }
+}
+
+function calculatePercentage() {
+  try {
+    currentInput = (eval(currentInput) / 100).toString();
+    display.innerText = currentInput;
+  } catch (error) {
+    display.innerText = "Error";
+  }
+}
+
+function toggleSign() {
+  try {
+    currentInput = (-eval(currentInput)).toString();
+    display.innerText = currentInput;
+  } catch (error) {
+    display.innerText = "Error";
+  }
+}
+
+
+function clearLastDigit() {
+  currentInput = currentInput.slice(0, -1);
+  display.innerText = currentInput === "" ? "0" : currentInput;
+}
+
+
+
 function calculate() {
   try {
     currentInput = eval(currentInput).toString();
